f0d9f83398f3598783d6f8c9b52f32c29dd4f71b
PramodKumavat7758 <78492409+PramodKumavat7758@users.noreply.github.com>
2024-01-08 20:05:33 +0530

    Quize Creation support adding

--- QuizeApp/src/main/java/com/Pramod/QuizessApp/DAO/QuestionDao.java
+++ QuizeApp/src/main/java/com/Pramod/QuizessApp/DAO/QuestionDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
+
     @Query(value = "SELECT * FROM Question q WHERE q.category = ?1 ORDER BY RAND() LIMIT ?2", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
